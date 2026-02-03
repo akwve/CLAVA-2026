16bebe376fb65548af0a4fb05cd9b4c5615dd230
akwve <ay.suganuma@gmail.com>
2026-02-02 16:35:05 -0800

    modified page.py to handle page capacity and bytes tracking

--- lstore/page.py
+++ lstore/page.py
@@ -6,9 +6,16 @@ class Page:
         self.data = bytearray(4096)
 
     def has_capacity(self):
-        pass
+        # check if page have not reached capacity
+        if self.num_records < 512:
+            return True
+        return False
+        #pass
 
     def write(self, value):
         self.num_records += 1
+        # track on bytes
+        self.data[self.num_records * 8 - 8:self.num_records * 8] = value.to_bytes(8)
+        #
         pass
 
