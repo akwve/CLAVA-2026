cc5b3e080606aeddc938e16ce69977f1b1bc2bab
akwve <ay.suganuma@gmail.com>
2026-02-05 21:01:02 -0800

    add functions and features necessary for insert function in query.py

--- lstore/table.py
+++ lstore/table.py
@@ -9,10 +9,30 @@ SCHEMA_ENCODING_COLUMN = 3
 
 class Record:
 
-    def __init__(self, rid, key, columns):
-        self.rid = rid
+    def __init__(self, rid, key, columns, schema_encoding):
+        # primary key
         self.key = key
+
+        # detail context for each columns
         self.columns = columns
+
+        # Metadata Columns
+
+        # 0: indirection column 
+        # base page: stores RID of latest tail record
+        # tail page: stores the RID of the previous tail record
+        self.indirection = None
+
+        # 1: RID column
+        self.rid = rid
+
+        # 2: timestamp column
+        self.timestamp = time()
+
+        # 3: schema encoding column
+        # base page: track total updates
+        # tail page: track how many columns have been updated in that update
+        self.schema_encoding = schema_encoding
 
 class Table:
 
@@ -25,20 +45,29 @@ class Table:
         self.name = name
         self.key = key
         self.num_columns = num_columns
-        self.page_directory = {}
+        self.page_directory = {} # list (rid, page, offset)
         self.index = Index(self)
         self.merge_threshold_pages = 50  # The threshold to trigger a merge
-
+        self.total_columns = 4 + num_columns # metadata column + given columns
+        
         # base pages
-        self.base_page_range = 0
-        self.base_page = 0
+        self.base_page_range = [0] * self.total_columns # track range index for each columns
+        self.base_pages = [[] for j in range(self.total_columns)] # store list of base pages for each columns
 
         # tail pages
-        self.tail_page_range = 0
-        self.tail_page = 0
-        #pass
+        self.tail_page_range = [0] * self.total_columns # track range index for each columns
+        self.tail_pages = [[] for j in range(self.total_columns)] # store list of tail pages for each columns
+
+        # count the RID for its uniqueness
+        self.rid_count = 0
+
+        
 
     def __merge(self):
         print("merge is happening")
         pass
- 
+    
+    # return a new unique RID
+    def get_unique_rid(self):
+        self.rid_count += 1
+        return self.rid_count
