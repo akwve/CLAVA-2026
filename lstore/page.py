705c1185538ea638a35a0f7b6dcdf08724d70a02
akwve <ay.suganuma@gmail.com>
2026-02-05 21:00:05 -0800

    add comment for clarification

--- lstore/page.py
+++ lstore/page.py
@@ -6,16 +6,19 @@ class Page:
         self.data = bytearray(4096)
 
     def has_capacity(self):
-        # check if page have not reached capacity
+        # check if page have not reached capacity of 4096/8 = 512
+        # page of 512 records for each
         if self.num_records < 512:
             return True
         return False
-        #pass
+        
 
     def write(self, value):
         self.num_records += 1
         # track on bytes
         self.data[self.num_records * 8 - 8:self.num_records * 8] = value.to_bytes(8)
-        #
-        pass
+        
+        # return the index where the value is stored
+        return self.num_records - 1
+
 
