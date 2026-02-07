49a42d5f10f903e87470ab93fe309ec15163e269
akwve <ay.suganuma@gmail.com>
2026-02-06 18:04:19 -0800

    removed self.base_page_range and self.tail_page_range, as they are redundant

--- lstore/table.py
+++ lstore/table.py
@@ -45,29 +45,33 @@ class Table:
         self.name = name
         self.key = key
         self.num_columns = num_columns
-        self.page_directory = {} # list (rid, page, offset)
+        self.page_directory = {} # list (page, offset)
         self.index = Index(self)
         self.merge_threshold_pages = 50  # The threshold to trigger a merge
         self.total_columns = 4 + num_columns # metadata column + given columns
-        
+
         # base pages
-        self.base_page_range = [0] * self.total_columns # track range index for each columns
-        self.base_pages = [[] for j in range(self.total_columns)] # store list of base pages for each columns
+        #self.base_page_range = [[0]] * self.total_columns # track range and index for each columns
+        self.base_pages = [[[]] for j in range(self.total_columns)] # store list of base pages for each columns: [column][range_index (16 max)][page_index (512 max)]
 
         # tail pages
-        self.tail_page_range = [0] * self.total_columns # track range index for each columns
-        self.tail_pages = [[] for j in range(self.total_columns)] # store list of tail pages for each columns
+        #self.tail_page_range = [0] * self.total_columns # track range index for each columns
+        self.tail_pages = [[] for j in range(self.total_columns)] # store list of tail pages for each columns: [column][page_index]
 
         # count the RID for its uniqueness
-        self.rid_count = 0
+        self.rid_count = [0, 1] # [base RID count, tail RID count]
 
-        
 
     def __merge(self):
         print("merge is happening")
         pass
     
-    # return a new unique RID
-    def get_unique_rid(self):
-        self.rid_count += 1
-        return self.rid_count
+    # return a new unique RID: base RID starts with 'b', tail RID starts with 't'
+    def get_unique_rid(self, base=False):
+        if base:
+            self.rid_count[0] += 1
+            return ('b' + str(self.rid_count[0]))
+        else:
+            self.rid_count[1] += 1
+            return ('t' + str(self.rid_count[1]))
+        
